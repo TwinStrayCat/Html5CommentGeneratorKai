21be0368bdc0376b6fda1371bb57c67aef00972d
彼岸扇花 <47345982+TwinStrayCat@users.noreply.github.com>
2019-04-04 16:55:21 +0900

    Delete ajax.php

--- ajax.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(isset($_GET['url']) && preg_match('/^https?:\/\/gyazo/', $_GET['url'])){
-    $endpoint = "https\:\/\/api\.gyazo\.com\/api\/oembed";
-    echo file_get_contents($endpoint."\?url\=".$_GET['url']);
-}
-?>
