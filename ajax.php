e418319ef738bc9bae9a21125f52a68bf436642b
彼岸扇花 <47345982+TwinStrayCat@users.noreply.github.com>
2019-04-04 16:48:24 +0900

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,10 +1,6 @@
 <?php
 if(isset($_GET['url']) && preg_match('/^https?:\/\/gyazo/', $_GET['url'])){
     $endpoint = "https\:\/\/api\.gyazo\.com\/api\/oembed";
-    $curl = curl_init();
-    curl_setopt($curl,CURLOPT_URL,$endpoint."\?url\=".$_GET['url']);
-    $res = curl_exec($curl);
-    curl_close($curl);
-    echo json_encode($res);
+    echo file_get_contents($endpoint."\?url\=".$_GET['url']);
 }
 ?>
